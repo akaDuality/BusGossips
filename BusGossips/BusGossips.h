449fdd0c034534f190a332cc8616914223b3ddea
Mikhail Rubanov <akaDuality@yandex.ru>
2019-08-22 14:57:16 +0500

    Initial Commit

---
+++ BusGossips/BusGossips.h
@@ -0,0 +1,19 @@
+//
+//  BusGossips.h
+//  BusGossips
+//
+//  Created by Mikhail Rubanov on 22/08/2019.
+//  Copyright © 2019 akaDuality. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BusGossips.
+FOUNDATION_EXPORT double BusGossipsVersionNumber;
+
+//! Project version string for BusGossips.
+FOUNDATION_EXPORT const unsigned char BusGossipsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BusGossips/PublicHeader.h>
+
+
