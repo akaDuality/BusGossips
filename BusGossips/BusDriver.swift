98b58f4f99fe37582586fb9fbb4a5c20ec9ed0fc
Mikhail Rubanov <akaDuality@yandex.ru>
2019-08-22 15:01:02 +0500

    start

---
+++ BusGossips/BusDriver.swift
@@ -0,0 +1,9 @@
+//
+//  BusDriver.swift
+//  BusGossips
+//
+//  Created by Mikhail Rubanov on 22/08/2019.
+//  Copyright © 2019 akaDuality. All rights reserved.
+//
+
+import Foundation
--- BusGossipsTests/BusGossipsTests.swift
+++ BusGossipsTests/BusGossipsTests.swift
@@ -9,26 +9,21 @@
 import XCTest
 @testable import BusGossips
 
+// ver 1
+// 3 1 2 3
+// 3 2 3 1
+// 4 2 3 4 5
+// output: 5
+
+// ver 2
+// 2 1 2
+// 5 2 8
+// output: never
+
 class BusGossipsTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+     
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
